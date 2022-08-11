b3e860b3a5d17dd8a2c26dbc7e962205f1d4204c
Dominic Giacona <dominic@exobyte.io>
2022-08-11 01:23:12 -0400

    added countries

---
+++ api/country.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    message = self.path
+    # message = ""
+    self.wfile.write(message.encode())
+    return
