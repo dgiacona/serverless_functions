c06b0484eb645983271ac56a48addf07a5d11a50
Dominic Giacona <dominic@exobyte.io>
2022-08-11 01:41:05 -0400

    added logic

--- api/country.py
+++ api/country.py
@@ -7,7 +7,11 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    message = self.path
-    # message = ""
+    url_components = parse.urlsplit(self.path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    query_dict = dict(query_string_list)
+    country = query_dict["country"]
+    message = f"Nice {country}"
+
     self.wfile.write(message.encode())
     return
