b9769c18d427728c23774b2e2de6df078aece738
Dominic Giacona <dominic@exobyte.io>
2022-08-11 02:07:40 -0400

    completed demo follow along

--- api/aloha.py
+++ api/aloha.py
@@ -10,8 +10,8 @@ class handler(BaseHTTPRequestHandler):
     url_components = parse.urlsplit(self.path)
     query_string_list = parse.parse_qsl(url_components.query)
     query_dict = dict(query_string_list)
-    country = query_dict["country"]
-    message = f"Nice {country}"
+    friend = query_dict["friend"]
+    message = f"Nice {friend}"
 
     self.wfile.write(message.encode())
     return
